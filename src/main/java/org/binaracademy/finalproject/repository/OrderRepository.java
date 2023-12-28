d45d74db61dc5613b91cdf7aef4b540474ba8787
Muhamad Bintang Alfarizki <muhamadbintangalfarizki@gmail.com>
2023-12-28 15:05:19 +0700

    Update 28/12/2023 (Order fixed 2)

--- src/main/java/org/binaracademy/finalproject/repository/OrderRepository.java
+++ src/main/java/org/binaracademy/finalproject/repository/OrderRepository.java
@@ -1,6 +1,5 @@
 package org.binaracademy.finalproject.repository;
 
-import org.binaracademy.finalproject.model.Course;
 import org.binaracademy.finalproject.model.Order;
 import org.binaracademy.finalproject.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,13 +11,9 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface OrderRepository extends JpaRepository<Order, String> {
 
-    Boolean existsByUsers(Users users);
-
-    Boolean existsByCourse(Course course);
-
     @Modifying
     void deleteByUsers(Users users);
 
-    @Query(nativeQuery = true, value = "select * from orders o where o.user_id = :userId and o.course_id = :courseId")
+    @Query(nativeQuery = true, value = "select count(*) > 0 from orders o where o.user_id = :userId and o.course_id = :courseId")
     Boolean orderValidation(@Param("userId") Long id, @Param("courseId") String courseId);
 }
